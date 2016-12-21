e4ef5ccb6386a28bee6bb215f92f6215dc606294
Mehmet Gerceker <mgerceker@nerdwallet.com>
2016-12-21 13:01:09 -0800

    Removes mobile devices.

--- conftest.py
+++ conftest.py
@@ -7,38 +7,6 @@ from selenium.webdriver.remote.remote_connection import RemoteConnection
 
 browsers = [
     {
-        'browserName': 'android',
-        'platform': 'Linux',
-        'version': '4.3',
-        'deviceName': 'Android Emulator',
-        'deviceOrientation': 'portrait',
-        'name': 'Python Selenium Android 4.3 example'
-    },
-    {
-        'browserName': 'android',
-        'platform': 'Linux',
-        'version': '5.1',
-        'deviceName': 'Android Emulator',
-        'deviceOrientation': 'portrait',
-        'name': 'Python Selenium Android 5.1 example'
-    },
-    {
-
-        'browserName': 'iPhone',
-        'platform': 'Mac 10.10',
-        'version': '9.2',
-        'deviceName': 'iPhone 6',
-        'deviceOrientation': 'portrait',
-        'name': 'Python Selenium iOS 9.2 example'
-    },
-    {
-        'browserName': 'iPhone',
-        'platform': 'Mac 10.10',
-        'platformVersion': '8.4',
-        'deviceName': 'iPhone 6',
-        'deviceOrientation': 'portrait',
-        'name': 'Python Selenium iPhone 6 iOS 8.4 example'
-    }, {
 
         "platform": "Windows 10",
         "browserName": "internet explorer",
@@ -48,7 +16,9 @@ browsers = [
         "platform": "OS X 10.11",
         "browserName": "safari",
         "version": "9"
-    }]
+    }
+]
+
 
 def pytest_generate_tests(metafunc):
     if 'driver' in metafunc.fixturenames:
